168272a49d78bc9f5b4e7c11a6a02ea92b38ba8b
karmasakshi <karmasakshi@users.noreply.github.com>
2024-10-17 02:00:37 +0530

    feat(general): add dashboard content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import { usePortfolioStore } from '@cryptack/store/portfolio';
+
 const Dashboard = () => {
+  const { portfolio } = usePortfolioStore();
+
   return (
     <div>
       <h1>Portfolio Summary</h1>
+      <p>Value: USD {portfolio.value.toFixed(2)}</p>
+      <p>Holdings: {portfolio.holdings.length}</p>
     </div>
   );
 };
