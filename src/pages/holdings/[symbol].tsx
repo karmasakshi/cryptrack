7ce2a627fb4612c5e85974a7dac0e067910f6e0c
karmasakshi <karmasakshi@users.noreply.github.com>
2024-10-17 02:01:19 +0530

    feat(general): add holding content

--- src/pages/holdings/[symbol].tsx
+++ src/pages/holdings/[symbol].tsx
@@ -1,9 +1,40 @@
+import { usePortfolioStore } from '@cryptack/store/portfolio';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
 const Holding = () => {
-  return (
-    <div>
-      <h1>Holding</h1>
-    </div>
-  );
+  const router = useRouter();
+  const { symbol } = router.query;
+  const { portfolio } = usePortfolioStore();
+  const [historicalData, setHistoricalData] = useState([]);
+
+  const cryptocurrency = portfolio.holdings.find((c) => c.symbol === symbol);
+
+  useEffect(() => {
+    if (cryptocurrency) {
+      setHistoricalData([]);
+    }
+  }, [cryptocurrency]);
+
+  if (!cryptocurrency) {
+    return <div>Cryptocurrency not found.</div>;
+  } else {
+    return (
+      <div>
+        <h1>{cryptocurrency.name} Details</h1>
+        <p>Price: {cryptocurrency.price}</p>
+        <h2>Historical Prices</h2>
+        <ul>
+          {historicalData.map((data, index) => (
+            <li key={index}>
+              Date: {new Date(data.time * 1000).toLocaleDateString()}, Price: $
+              {data.close}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default Holding;
