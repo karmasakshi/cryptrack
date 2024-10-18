3047050148bd9fc6be0341d15b7ed4c9dd367e5d
karmasakshi <karmasakshi@users.noreply.github.com>
2024-10-19 01:39:56 +0530

    refactor(general): add caching

--- src/apis/cryptocompare.ts
+++ src/apis/cryptocompare.ts
@@ -1,9 +1,18 @@
-import axios from 'axios';
+import axios, { AxiosAdapter } from 'axios';
+import { setupCache } from 'axios-cache-adapter';
 
 const API_KEY = process.env.NEXT_PUBLIC_CRYPTOCOMPARE_API_KEY;
 
+const cache = setupCache({
+  maxAge: 15 * 60 * 1000, // Cache for 15 minutes
+});
+
+const axiosInstance = axios.create({
+  adapter: cache.adapter as AxiosAdapter,
+});
+
 export const getCryptocurrencyPrice = async (symbol: string) => {
-  const response = await axios.get(
+  const response = await axiosInstance.get(
     `https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD`,
     {
       headers: {
@@ -15,7 +24,7 @@ export const getCryptocurrencyPrice = async (symbol: string) => {
 };
 
 export const getCryptocurrencyData = async (symbol: string) => {
-  const response = await axios.get(
+  const response = await axiosInstance.get(
     `https://data-api.cryptocompare.com/asset/v1/data/by/symbol?asset_symbol=${symbol}`,
     {
       headers: {
@@ -27,7 +36,7 @@ export const getCryptocurrencyData = async (symbol: string) => {
 };
 
 export const getHistoricalData = async (symbol: string, days: number) => {
-  const response = await axios.get(
+  const response = await axiosInstance.get(
     `https://min-api.cryptocompare.com/data/v2/histoday?fsym=${symbol}&tsym=USD&limit=${days}`,
     {
       headers: {
